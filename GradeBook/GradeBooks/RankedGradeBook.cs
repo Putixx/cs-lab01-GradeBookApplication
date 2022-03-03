3ad52901320340fae0e994469095a13d3f9538d4
Putixx <80105950+Putixx@users.noreply.github.com>
2022-03-03 21:08:30 +0100

    Override RankedGradeBook's GetLetterGrade method start + Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,43 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int counter = 0;
+
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            foreach(Student s in Students)
+            {
+                if(s.AverageGrade > averageGrade)
+                    counter++;
+
+                else
+                {
+                    if ((counter / Students.Count) <= 0.2)
+                        return 'A';
+                    else if ((counter / Students.Count) <= 0.4)
+                        return 'B';
+                    else if ((counter / Students.Count) <= 0.6)
+                        return 'C';
+                    else if ((counter / Students.Count) <= 0.8)
+                        return 'D';
+                    else
+                        return 'F';
+                }
+            }
+
+            return ' ';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
     }
 }
